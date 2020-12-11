585cd58d3fc7b0128ee1aa8a5d6a6a894c5851c6
Arne Recknagel <recknagel@auxmoney.com>
2020-12-11 17:09:04 +0100

    day 10, still buggy, but nearly done I hope

--- AoC20/day_10/b.py
+++ AoC20/day_10/b.py
@@ -1,6 +1,9 @@
+from functools import reduce, lru_cache
+
 from AoC20.day_10 import example_data_b as data
 
 
+@lru_cache(maxsize=None)
 def weird_fib(n):
     if n == 0:
         return 1
@@ -11,14 +14,19 @@ def weird_fib(n):
     return weird_fib(n-1) + weird_fib(n-2) + weird_fib(n-3)
 
 
-def edge_distances(inp):
-    for a, b in zip(inp, inp[1:]):
-        yield b-a
-
-
-def group_singles(distances):
-    ...
+def group_singles(inp):
+    edge_distances = (b-a for a, b in zip(inp, inp[1:]))
+    current = 0
+    while foo := next(edge_distances, False):
+        if foo == 3:
+            if current:
+                yield current
+                current = 0
+        if foo == 1:
+            current += 1
 
 
 data = sorted(map(int, data))
 data = [0, *data, data[-1]+3]
+
+print(reduce(int.__mul__, group_singles(data)))
