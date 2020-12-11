e60a09653c5f317b93064d9d11c969e46591eaaf
Arne <arecknag@gmail.com>
2020-12-11 17:24:49 +0100

    day 10, finally done. No re-usage between a and b, but my brain is fried, so..

--- AoC20/day_10/__init__.py
+++ AoC20/day_10/__init__.py
@@ -1,5 +1,3 @@
-
-
 example_data_a = """\
 16
 10
--- AoC20/day_10/b.py
+++ AoC20/day_10/b.py
@@ -1,32 +1,30 @@
 from functools import reduce, lru_cache
 
-from AoC20.day_10 import example_data_b as data
+from AoC20.day_10 import data as data
 
 
 @lru_cache(maxsize=None)
 def weird_fib(n):
     if n == 0:
-        return 1
+        return 1  # makes sense?
     if n == 1:
-        return 2
+        return 1
     if n == 2:
-        return 4
+        return 2
     return weird_fib(n-1) + weird_fib(n-2) + weird_fib(n-3)
 
 
 def group_singles(inp):
-    edge_distances = (b-a for a, b in zip(inp, inp[1:]))
-    current = 0
-    while foo := next(edge_distances, False):
-        if foo == 3:
-            if current:
-                yield current
-                current = 0
-        if foo == 1:
-            current += 1
+    group_size = 0
+    for distance in (b-a for a, b in zip(inp, inp[1:])):
+        if distance == 3:
+            yield group_size
+            group_size = 0
+        if distance == 1:
+            group_size += 1
 
 
 data = sorted(map(int, data))
 data = [0, *data, data[-1]+3]
 
-print(reduce(int.__mul__, group_singles(data)))
+print(reduce(int.__mul__, map(weird_fib, group_singles(data))))
